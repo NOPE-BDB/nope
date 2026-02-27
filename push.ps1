311fac65f97b5bb57f179c2b3cf016730dc3e3a2
NOPE-BDB <3241410869@qq.com>
2026-02-27 23:17:44 +0800

    Fix empty game list issue

--- push.ps1
+++ push.ps1
@@ -1,5 +1,5 @@
 cd c:\Users\witma\Documents\TRAE\railway
 $env:Path = "C:\Program Files\Git\bin;$env:Path"
 git add .
-git commit -m "Fix game list refresh issue"
+git commit -m "Fix empty game list issue"
 git push -u origin main --force
