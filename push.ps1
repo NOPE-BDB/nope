36eea6b8329e0fd11837c8269c2e83e55cf86417
NOPE-BDB <3241410869@qq.com>
2026-02-28 01:05:26 +0800

    Add upload game page modal

--- push.ps1
+++ push.ps1
@@ -1,5 +1,5 @@
 cd c:\Users\witma\Documents\TRAE\railway
 $env:Path = "C:\Program Files\Git\bin;$env:Path"
-git add -A
-git commit -m "Upload button fix"
+git add .
+git commit -m "Add upload game page modal"
 git push https://github.com/NOPE-BDB/nope.git main --force
