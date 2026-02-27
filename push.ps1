a751acd06e0393ef181e9eccd2527c214922fd61
NOPE-BDB <3241410869@qq.com>
2026-02-28 00:58:39 +0800

    Add upload button

--- push.ps1
+++ push.ps1
@@ -1,5 +1,5 @@
 cd c:\Users\witma\Documents\TRAE\railway
 $env:Path = "C:\Program Files\Git\bin;$env:Path"
 git add .
-git commit -m "Fix: Upload button shows form on click"
+git commit -m "Add upload button"
 git push -u origin main --force
