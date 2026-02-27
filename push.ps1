e0e7af89f3c9ea9e866328449f937dc01e5fdaed
NOPE-BDB <3241410869@qq.com>
2026-02-28 01:12:22 +0800

    Add file upload, cover, intro, rename menus

--- push.ps1
+++ push.ps1
@@ -1,5 +1,5 @@
 cd c:\Users\witma\Documents\TRAE\railway
 $env:Path = "C:\Program Files\Git\bin;$env:Path"
 git add .
-git commit -m "Add upload game page modal"
+git commit -m "Add file upload, cover, intro, rename menus"
 git push https://github.com/NOPE-BDB/nope.git main --force
