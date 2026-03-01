fc84bc22de1036e53a98dd0be3a01ea38c3c9b2e
NOPE-BDB <3241410869@qq.com>
2026-03-01 23:43:30 +0800

    Admin modal color and follow button animation

--- push.ps1
+++ push.ps1
@@ -1,4 +1,4 @@
 cd c:\Users\witma\Documents\TRAE\railway
 git add .
-git commit -m "Fix follow button and modal color"
+git commit -m "Admin modal color and follow button animation"
 git push https://github.com/NOPE-BDB/nope.git main --force
