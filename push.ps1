7e447b15bef50a3cf94ded1a000f3430ea1e5b65
NOPE-BDB <3241410869@qq.com>
2026-03-01 23:49:13 +0800

    Fix follow button not updating

--- push.ps1
+++ push.ps1
@@ -1,4 +1,4 @@
 cd c:\Users\witma\Documents\TRAE\railway
 git add .
-git commit -m "Admin modal color and follow button animation"
+git commit -m "Fix follow button not updating"
 git push https://github.com/NOPE-BDB/nope.git main --force
